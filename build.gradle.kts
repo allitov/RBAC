0a22d1b1ad695d204b950c33b0ee01c1b5336d2a
Alexander Litovkin <litovkinalexan@gmail.com>
2026-02-10 19:56:17 +0700

    chore: add spotless
    - The code style is automatically applied when you commit.
    - Palantir Java Format is used.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
     id("java")
+    id("com.diffplug.spotless") version "8.2.1"
 }
-
-group = "io.allitov"
-version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -15,6 +13,51 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+configure<JavaPluginExtension> {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    java {
+        target("src/**/*.java")
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        endWithNewline()
+        palantirJavaFormat("2.86.0")
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.register<Copy>("copyGitHooks") {
+    description = "Copies git hooks from project directory to .git/hooks"
+    group = "git"
+
+    val gitDir = File("$rootDir/.git")
+
+    onlyIf { gitDir.exists() }
+
+    from("$rootDir/git/hooks")
+    into("${gitDir.path}/hooks")
+
+    filePermissions {
+        user { read = true; write = true; execute = true }
+
+        group { read = true; execute = true }
+
+        other { read = true; execute = true }
+    }
+
+    doLast {
+        println("Git hooks installed successfully into ${gitDir.path}/hooks")
+        file("$rootDir/git/hooks").listFiles()?.forEach {
+            println("   -> Enabled hook: ${it.name}")
+        }
+    }
+}
+
+tasks.maybeCreate("prepareKotlinBuildScriptModel").dependsOn("copyGitHooks")
